ea02d01354e959eeaa237bc7448186d9733acf2d
Figaro4545 <wasumin454@gmail.com>
2021-07-22 10:38:58 +0300

    create project

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    
+    static path = '/';
+    
+    render() {
+        return (
+            <h1>Приветики111111</h1>
+        );
+    }
+    
+}
