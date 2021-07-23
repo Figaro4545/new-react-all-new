56ddf2d33959328a35515cb0c36e9d2bb3366e52
Figaro4545 <wasumin454@gmail.com>
2021-07-23 11:09:29 +0300

    hi

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Header } from './components/index';
 import { DevTools } from './utils/index';
 
 export default class App extends React.Component {
@@ -8,7 +9,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <Header />
                 { process.env.NODE_ENV !== 'production' ? <DevTools /> : null }
             </div>
         );
